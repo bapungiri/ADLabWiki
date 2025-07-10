e882c43d02a66bf03e4a1003b6461ff0473f6c44
bapungiri <bapungiri.codespace@outlook.com>
2025-07-10 09:51:16 +0530

    Quartz sync: Jul 10, 2025, 9:51 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://bapungiri.github.io/ADLabWiki/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
